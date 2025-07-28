f3cc476a187fcec0ecdbe02fe1a56207d474fedf
HrishiKShah28 <hrishi28june@gmail.com>
2025-07-29 04:19:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import plotly.express as px
 import traceback
-
+import cv2
 # Import your existing modules with error handling
 try:
     from main import (
